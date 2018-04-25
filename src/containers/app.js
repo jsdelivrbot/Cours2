f87881522ea072cfff05fe595272e42291e57aaf
Ski3L <38662758+Ski3L@users.noreply.github.com>
2018-04-25 22:02:12 +0200

    up
    MàJ

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,10 +1,11 @@
 import React from "react";
 import SearchBar from "../components/search-bar";
-
+import VideoList from "./video-list";
 const App = function() {
   return (
     <div>
       <SearchBar />
+      <VideoList />
     </div>
   );
 };
